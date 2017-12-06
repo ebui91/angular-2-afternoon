c06744705277f468411eb1a1f2d5521a7fa277bb
Eric Bui <ericbui2@gmail.com>
2017-12-06 15:04:19 -0600

    finished travel app

---
+++ app/locations/locationsCtrl.js
@@ -0,0 +1,6 @@
+angular.module('devmtnTravel').controller('locationsCtrl', function($scope, mainSrvc){
+  mainSrvc.getTravelInfo().then(response=> {
+    $scope.locations= response.data;
+    console.log($scope.locations);
+  })
+});
